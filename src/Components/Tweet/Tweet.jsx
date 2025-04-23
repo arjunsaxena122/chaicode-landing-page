aa7ca15be814d2931ce54428630de5c31265c9a4
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-04-24 01:15:48 +0530

    CHANGE: change button

--- src/Components/Tweet/Tweet.jsx
+++ src/Components/Tweet/Tweet.jsx
@@ -7,11 +7,13 @@ function Tweet() {
       <p className="my-1 px-2 py-2">Love that we get from our community</p>
       <h3 className="text-5xl px-2 py-2">Tweet Love</h3>
       <div className="flex justify-center items-center gap-20 py-8 px-8 my-10">
-        <TweetCard heading={"Radhika gupta"}/>
-        <TweetCard heading={"Krishna Sharma"}/>
-        <TweetCard heading={"Dhiru Bhai"}/>
+        <TweetCard heading={"Radhika gupta"} />
+        <TweetCard heading={"Krishna Sharma"} />
+        <TweetCard heading={"Dhiru Bhai"} />
       </div>
-      <HeroButton text={"Join Cohorts Live Classes"}/>
+      <button className="flex justify-center items-center border px-4 py-2 rounded bg-linear-to-r from-orange-600 to-[#F97316] border-none mb-10 cursor-pointer">
+        Join all live cohorts
+      </button>
     </section>
   );
 }
