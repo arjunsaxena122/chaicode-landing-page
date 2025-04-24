55861a196b0b15a48551d751e9a1eb9f22e6af5d
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-04-25 02:42:25 +0530

    ADD: adding App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import HeroPage from "./Components/HeroPage/HeroPage"
-import Navbar from "./Components/Navbar/Navbar"
-import Tweet from "./Components/Tweet/Tweet"
+import AppDownload from "./Components/AppDownload/AppDownload";
+import BenefitsCohort from "./Components/BenefitsCohort/BenefitsCohort";
+import Community from "./Components/Community/Community";
+import Feature from "./Components/Feature/Feature";
+import HeroPage from "./Components/HeroPage/HeroPage";
+import LiveTrain from "./Components/LiveTrain/LiveTrain";
+import Navbar from "./Components/Navbar/Navbar";
+import OpenSource from "./Components/OpenSource/OpenSource";
+import TagSection from "./Components/TagSection/TagSection";
+import Testimonial from "./Components/Testimonial/Testimonial";
+// import ProofCard from "./Components/ProofCard/ProofCard"
+import Tweet from "./Components/Tweet/Tweet";
+// import Udemy from "./Components/Udemy/Udemy";
 
 function App() {
-
   return (
-    <div >
-     <Navbar/>
-     <HeroPage/>
-     <Tweet/>
-    </div>
-  )
+    <>
+      <Navbar />
+      <HeroPage />
+      <Tweet />
+      {/* <ProofCard /> */}
+      <LiveTrain />
+      <Testimonial />
+      {/* <Udemy /> */}
+      <BenefitsCohort />
+      <Feature />
+      <TagSection />
+      <Community />
+      <OpenSource />
+      <AppDownload />
+    </>
+  );
 }
 
-export default App
+export default App;
