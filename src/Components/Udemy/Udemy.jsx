f2cb5050a411fa75b5e8410c77b0eb3528415b76
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-04-26 11:06:02 +0530

    ADD: adding udemy component

---
+++ src/Components/Udemy/Udemy.jsx
@@ -0,0 +1,10 @@
+function Udemy() {
+  return (
+    <section className="flex justify-center items-center flex-col">
+      <h3 className="text-5xl my-2">Udemy</h3>
+      <p>Not only in India, We are global leaders in teach education</p>
+    </section>
+  );
+}
+
+export default Udemy;
