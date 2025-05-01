d21dc49e97aaeb3996e6b0c05f95ca208c25deb4
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-05-01 09:11:48 +0530

    FIX: spacing and margin between the cards

--- src/Components/BenefitsCohort/BenefitsCard.jsx
+++ src/Components/BenefitsCohort/BenefitsCard.jsx
@@ -1,18 +1,19 @@
 function BenefitsCard({ link, imgAlt, title, description }) {
-    return (
-      <div className="border rounded flex justify-center items-center my-10 mx-8">
-        <img
-          src={link}
-          alt={imgAlt}
-          className="w-20 border object-cover mx-4 my-3 px-2 py-1 rounded"
-        />
-        <div>
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
+  return (
+    <div className="border rounded-xl flex flex-col justify-start items-center px-6 py-8 border-slate-800 dark:bg-[#001618] hover:scale-105 transition-transform duration-300 shadow-sm shadow-amber-600 border-none">
+      <div className="w-[120px] h-[120px] mb-6 rounded-full overflow-hidden dark:bg-white border flex justify-center items-center ">
+        <img src={link} alt={imgAlt} className="w-full h-full object-contain" />
       </div>
-    );
-  }
-  
-  export default BenefitsCard;
-  
+
+      <h3 className="font-bold text-center tracking-wider text-xl text-[#F97316] mb-2">
+        {title}
+      </h3>
+
+      <p className="text-center dark:text-gray-300 text-sm text-black">
+        {description}
+      </p>
+    </div>
+  );
+}
+
+export default BenefitsCard;
