3c17c1fc717bb33e69c07965df8532c3fc81fd55
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-05-05 22:10:14 +0530

    FIX: dark and light mode of the cards

--- src/Components/Testimonial/TestimonialCard.jsx
+++ src/Components/Testimonial/TestimonialCard.jsx
@@ -2,7 +2,7 @@ import { Tweet, TweetSkeleton } from "react-tweet";
 
 function TestimonialCard({ id }) {
   return (
-    <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg border border-slate-700 overflow-hidden rounded-2xl flex flex-col items-center p-6 gap-4 bg-gray-900 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-y-95 hover:shadow-2xl hover:shadow-amber-500/50 shadow-lg">
+    <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg dark:border dark:border-slate-700 overflow-hidden rounded-2xl flex flex-col items-center p-6 gap-4 dark:bg-gray-900 cursor-pointer transition-all duration-300 ease-in-out shadow-amber-500/50 shadow-lg">
       <Tweet id={id} fallback={<TweetSkeleton />} />
     </div>
   );
