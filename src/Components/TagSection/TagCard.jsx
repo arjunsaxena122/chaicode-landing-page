b19c9ac52dc639d914e81508e828ce5481bcf47a
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-05-05 22:20:59 +0530

    FIX: remove border in light mode

--- src/Components/TagSection/TagCard.jsx
+++ src/Components/TagSection/TagCard.jsx
@@ -1,6 +1,6 @@
 function TagCard({ title }) {
   return (
-    <div className="w-full dark:border dark:border-gray-600 dark:bg-slate-950 px-4 py-5 flex justify-center items-center rounded-xl border shadow-md shadow-black transition-all duration-300 hover:scale-105">
+    <div className="w-full dark:border dark:border-gray-900 dark:bg-slate-950 px-4 py-5 flex justify-center items-center rounded-xl shadow-sm shadow-black transition-all duration-300 hover:scale-105">
       <h2 className="text-[#fd7616] font-bold text-base md:text-xl text-center">
         {title}
       </h2>
